5f228ff99afe820eb91170e0251cf1e2ab084feb
SethMegbetor <sethmegbetor@gmail.com>
2023-07-20 23:42:53 +0000

    done rewritting the main file

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,13 @@
 
 // import { AppModule } from './app/app.module';
 
-
 // platformBrowserDynamic().bootstrapModule(AppModule)
 //   .catch(err => console.error(err));
 
-import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
+import { AppModule } from './app/app.module';
 
 platformBrowserDynamic()
+  .bootstrapModule(AppModule)
+  .catch((err) => console.error(err));
