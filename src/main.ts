f959badb15f89a76d27b58eb597fd961bf4b8fa8
SethMegbetor <sethmegbetor@gmail.com>
2023-07-21 23:31:16 +0000

    enabling production mode

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,9 @@
 // platformBrowserDynamic().bootstrapModule(AppModule)
 //   .catch(err => console.error(err));
 
+// enabling production mode
+import { enableProdMode } from '@angular/core';
+
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
